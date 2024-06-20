3f1ee48d9c466daeb6caa3bfa22b23a1e4b9a9e2
Darvin18 <denisglebow2006@yandex.ru>
2024-06-20 22:14:39 +0300

    swiper_and_third-page

---
+++ swiperjs/swiper.js
@@ -0,0 +1,33 @@
+const gallary = new Swiper('.gallary', {
+    direction: 'horizontal',
+    loop: true,
+    spaceBetween: 50,
+    slidesPerView: 3,
+    slidesPerGroup: 3,
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    breakpoints: {
+        1251: {
+            spaceBetween: 50,
+            slidesPerView: 3,
+        },
+        951: {
+            spaceBetween: 50,
+            slidesPerView: 2,
+            slidesPerGroup: 2,
+        },
+        0: {
+            spaceBetween: 0,
+            slidesPerView: 1,
+            slidesPerGroup: 1,
+        },
+    },
+});
